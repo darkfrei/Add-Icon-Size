78d0a4961eab9b7ec290dfed9f9d75096dd98863
darkfrei <34188677+darkfrei@users.noreply.github.com>
2017-12-14 21:05:20 +0100

    Add files via upload

---
+++ data-final-fixes.lua
@@ -0,0 +1,36 @@
+log ('my turn')
+
+-- for prot_type_name, prot_type in pairs (data.raw) do
+	-- for prot_name, prot in pairs (prot_type) do
+    -- log ('data.raw["'.. prot_type_name .. '"]["' .. prot_name .. '"] = ' .. serpent.block(prot, {comment = false}))
+	-- end
+-- end
+
+item_type_list = {
+  "ammo",
+  "armor",
+  "gun",
+  "item",
+  "capsule",
+  "repair-tool",
+  "mining-tool",
+  "item-with-entity-data",
+  "rail-planner",
+  "tool",
+  "blueprint",
+  "deconstruction-item",
+  "blueprint-book",
+  "selection-tool",
+  "item-with-tags",
+  "item-with-label",
+  "item-with-inventory",
+  "module"
+}
+
+for _, item_type in pairs (item_type_list) do
+	for item_name, item_prot in pairs (data.raw[item_type]) do
+    if not item_prot.icon_size then
+      item_prot.icon_size = 32
+    end
+	end
+end
